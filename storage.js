0b989bf391163afbbd4a1e660e58e6a0eccb2c93
Metaflax <mstf.cobann1@gmail.com>
2023-06-01 11:01:19 +0300

    API

---
+++ storage.js
@@ -0,0 +1,32 @@
+class Storage {
+
+    static getSearchedUsersFromStorage(){
+        // tüm kullanıcaları al
+
+        let users;
+
+        if (localStorage.getItem("searched") === null){
+            users = [];
+        }
+        else {
+            users = JSON.parse(localStorage.getItem("searched"));
+        }
+        return users;
+    }
+    static addSearchedUserToStorage(username){
+        // Kullanıcı  ekle
+        let users = this.getSearchedUsersFromStorage();
+
+        // IndexOf
+
+        if(users.indexOf(username) === -1){
+            users.push(username);
+        }
+        localStorage.setItem("searched",JSON.stringify(users));
+    }
+    static clearAllSearchedUsersFromStorage(){
+        // Tüm kullanıcıları sil
+
+        localStorage.removeItem("searched");
+    }
+}
